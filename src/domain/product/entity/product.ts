521c80e4b1e75d3f135234f4400596e7d0713d9a
Matheus Silveira <blanchimaah@gmail.com>
2024-12-29 10:51:32 -0300

    notification pattern on product

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,49 +1,62 @@
-import ProductInterface from "./product.interface";
+import Entity from '../../@shared/entity/entity.abstract'
+import NotificationError from '../../@shared/notification/notification.error'
+import ProductInterface from './product.interface'
 
-export default class Product implements ProductInterface {
-  private _id: string;
-  private _name: string;
-  private _price: number;
+export default class Product extends Entity implements ProductInterface {
+  private _name: string
+  private _price: number
 
-  constructor(id: string, name: string, price: number) {
-    this._id = id;
-    this._name = name;
-    this._price = price;
-    this.validate();
+  constructor (id: string, name: string, price: number) {
+    super()
+    this._id = id
+    this._name = name
+    this._price = price
+    this.validate()
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors())
+    }
   }
 
-  get id(): string {
-    return this._id;
-  }
-  
-  get name(): string {
-    return this._name;
+  get id (): string {
+    return this._id
   }
 
-  get price(): number {
-    return this._price;
+  get name (): string {
+    return this._name
   }
 
-  changeName(name: string): void {
-    this._name = name;
-    this.validate();
+  get price (): number {
+    return this._price
   }
 
-  changePrice(price: number): void {
-    this._price = price;
-    this.validate();
+  changeName (name: string): void {
+    this._name = name
+    this.validate()
   }
 
-  validate(): boolean {
+  changePrice (price: number): void {
+    this._price = price
+    this.validate()
+  }
+
+  validate (): void {
     if (this._id.length === 0) {
-      throw new Error("Id is required");
+      this.notification.addError({
+        context: 'product',
+        message: 'Id is required'
+      })
     }
     if (this._name.length === 0) {
-      throw new Error("Name is required");
+      this.notification.addError({
+        context: 'product',
+        message: 'Name is required'
+      })
     }
     if (this._price < 0) {
-      throw new Error("Price must be greater than zero");
+      this.notification.addError({
+        context: 'product',
+        message: 'Price must be greater than zero'
+      })
     }
-    return true;
   }
 }
